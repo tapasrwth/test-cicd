da997cc39abdce7db5daa32c9b31b6abdda29121
Tapas Jana <54022681+tapasrwth@users.noreply.github.com>
2024-04-12 13:52:07 +0200

    Update set-version.sh

--- set-version.sh
+++ set-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit
 echo "======================================================="
-version_from_file=$(head -n 1 ./pipeline_script/semver.txt)
+version_from_file=$(head -n 1 ./version.txt)
 #app_version="${version_from_file/\#/}"
 app_version=$(echo ${version_from_file/?})
 export appVersion=${app_version}
